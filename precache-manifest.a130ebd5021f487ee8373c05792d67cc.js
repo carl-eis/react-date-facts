e806a82c6b2aca71ec2bf44f1d057a23fb554e07
Carl Eiserman <carleiserman@gmail.com>
2020-03-04 01:04:28 +0200

    Updates

---
+++ precache-manifest.a130ebd5021f487ee8373c05792d67cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64617f658c60813255abf0b85aa72cfd",
+    "url": "/react-date-facts/index.html"
+  },
+  {
+    "revision": "b77929d1388cb7b0b627",
+    "url": "/react-date-facts/static/css/2.37d27531.chunk.css"
+  },
+  {
+    "revision": "e7fa915e99b248099d8d",
+    "url": "/react-date-facts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b77929d1388cb7b0b627",
+    "url": "/react-date-facts/static/js/2.8b2bcfb7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react-date-facts/static/js/2.8b2bcfb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7fa915e99b248099d8d",
+    "url": "/react-date-facts/static/js/main.91039f71.chunk.js"
+  },
+  {
+    "revision": "25fbd8d3c699471778cf",
+    "url": "/react-date-facts/static/js/runtime-main.ab5d4993.js"
+  }
+]);
