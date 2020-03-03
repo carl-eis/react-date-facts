bba8d4d981ff2d5ec4529abe0547bbaad4bf01a2
Carl Eiserman <carleiserman@gmail.com>
2020-03-03 22:24:44 +0200

    feat: finished date info reducer

--- src/modules/date-info/reducer/index.ts
+++ src/modules/date-info/reducer/index.ts
@@ -1,4 +1,5 @@
 import { IReduxAction } from '../../../core/interfaces';
+import { FACT_FETCH_SUCCESS, DATE_RANGE_CHANGE, FACT_FETCH_ERROR } from '../actions';
 
 export interface IDateInfoReducerState {
   isLoadingFact: boolean;
@@ -17,6 +18,30 @@ const initialState: IDateInfoReducerState = {
 const dateInfoReducer = (state = initialState, action: IReduxAction): IDateInfoReducerState => {
   const { type, data } = action;
   switch (type) {
+    case DATE_RANGE_CHANGE: {
+      const { startDate, endDate } = data;
+      return {
+        ...state,
+        dateFact: 'loading ...',
+        isLoadingFact: true,
+        startDate,
+        endDate,
+      };
+    }
+    case FACT_FETCH_ERROR: {
+      return {
+        ...state,
+        dateFact: '',
+        isLoadingFact: false,
+      };
+    }
+    case FACT_FETCH_SUCCESS: {
+      return {
+        ...state,
+        dateFact: data,
+        isLoadingFact: false,
+      };
+    }
     default:
       return state;
   }
